6578d215e65503302e455f70725513c5b4a6b5e9
cyanray <zyanglin@outlook.com>
2023-07-25 05:28:09 +0800

    Add: Toolbar.

--- OccViewer.Viewer/D3DViewer.cs
+++ OccViewer.Viewer/D3DViewer.cs
@@ -4,133 +4,126 @@ using System.Windows.Media;
 using System.Windows.Interop;
 using System.Runtime.InteropServices;
 
-namespace IE_WPF_D3D
+namespace OccViewer.Viewer
 {
-  /// <summary>
-  /// Tool object for output OCCT rendering with Direct3D.
-  /// </summary>
-  class D3DViewer
-  {
-    /// <summary> Direct3D output image. </summary>
-    private D3DImage myD3DImage = new D3DImage ();
-
-    /// <summary> Direct3D color surface. </summary>
-    private IntPtr myColorSurf;
-
-    public OCCViewer Viewer;
-
-    /// <summary> Creates new Direct3D-based OCCT viewer. </summary>
-    public D3DViewer ()
+    /// <summary>
+    /// Tool object for output OCCT rendering with Direct3D.
+    /// </summary>
+    class D3DViewer
     {
-      myD3DImage.IsFrontBufferAvailableChanged
-        += new DependencyPropertyChangedEventHandler (OnIsFrontBufferAvailableChanged);
+        /// <summary> Direct3D output image. </summary>
+        private readonly D3DImage m_D3DImage = new();
 
-      BeginRenderingScene ();
-    }
+        /// <summary> Direct3D color surface. </summary>
+        private IntPtr m_ColorSurf;
 
-    /// <summary> Creates new Direct3D-based OCCT viewer. </summary>
-    private void OnIsFrontBufferAvailableChanged (object sender, DependencyPropertyChangedEventArgs e)
-    {
-      // If the front buffer is available, then WPF has just created a new
-      // Direct3D device, thus we need to start rendering our custom scene
-      if (myD3DImage.IsFrontBufferAvailable)
-      {
-        BeginRenderingScene ();
-      }
-      else
-      {
-        // If the front buffer is no longer available, then WPF has lost Direct3D
-        // device, thus we need to stop rendering until the new device is created
-        StopRenderingScene ();
-      }
-    }
+        private bool m_IsFailed = false;
 
-    private bool myIsFailed = false;
+        public OCCViewer? Viewer { get; private set; }
 
-    /// <summary> Initializes Direct3D-OCCT rendering. </summary>
-    private void BeginRenderingScene ()
-    {
-      if (myIsFailed)
-      {
-        return;
-      }
+        public D3DImage Image => m_D3DImage;
 
-      if (myD3DImage.IsFrontBufferAvailable)
-      {
-        Viewer = new OCCViewer();
-
-        if (!Viewer.InitViewer())
+        /// <summary> Creates new Direct3D-based OCCT viewer. </summary>
+        public D3DViewer()
         {
-          MessageBox.Show ("Failed to initialize OpenGL-Direct3D interoperability!",
-            "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            m_D3DImage.IsFrontBufferAvailableChanged
+              += new DependencyPropertyChangedEventHandler(OnIsFrontBufferAvailableChanged);
 
-          myIsFailed = true;
-          return;
+            BeginRenderingScene();
         }
 
-        // Leverage the Rendering event of WPF composition
-        // target to update the our custom Direct3D scene
-        CompositionTarget.Rendering += OnRendering;
-      }
-    }
-
-    /// <summary> Releases Direct3D-OCCT rendering. </summary>
-    public void StopRenderingScene ()
-    {
-      // This method is called when WPF loses its Direct3D device,
-      // so we should just release our custom Direct3D scene
-      CompositionTarget.Rendering -= OnRendering;
-      myColorSurf = IntPtr.Zero;
-    }
-
-    /// <summary> Performs Direct3D-OCCT rendering. </summary>
-    private void OnRendering (object sender, EventArgs e)
-    {
-      UpdateScene ();
-    }
-
-    /// <summary> Performs Direct3D-OCCT rendering. </summary>
-    private void UpdateScene ()
-    {
-      if (!myIsFailed
-        && myD3DImage.IsFrontBufferAvailable
-        && myColorSurf != IntPtr.Zero
-        && (myD3DImage.PixelWidth != 0 && myD3DImage.PixelHeight != 0))
-      {
-        myD3DImage.Lock ();
+        /// <summary> Creates new Direct3D-based OCCT viewer. </summary>
+        private void OnIsFrontBufferAvailableChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-          // Update the scene (via a call into our custom library)
-          Viewer.View.RedrawView ();
-
-          // Invalidate the updated region of the D3DImage
-          myD3DImage.AddDirtyRect(new Int32Rect(0, 0, myD3DImage.PixelWidth, myD3DImage.PixelHeight));
+            if (m_D3DImage.IsFrontBufferAvailable)
+            {
+                // If the front buffer is available, then WPF has just created a new
+                // Direct3D device, thus we need to start rendering our custom scene
+                BeginRenderingScene();
+            }
+            else
+            {
+                // If the front buffer is no longer available, then WPF has lost Direct3D
+                // device, thus we need to stop rendering until the new device is created
+                StopRenderingScene();
+            }
         }
-        myD3DImage.Unlock ();
-      }
-    }
 
-    /// <summary> Resizes Direct3D surfaces and OpenGL FBO. </summary>
-    public void Resize (int theSizeX, int theSizeY)
-    {
-      if (!myIsFailed && myD3DImage.IsFrontBufferAvailable)
-      {
-        // Set the back buffer for Direct3D WPF image
-        myD3DImage.Lock ();
+        /// <summary> Initializes Direct3D-OCCT rendering. </summary>
+        private void BeginRenderingScene()
         {
-          myD3DImage.SetBackBuffer (D3DResourceType.IDirect3DSurface9, IntPtr.Zero);
-          myColorSurf = Viewer.View.ResizeBridgeFBO (theSizeX, theSizeY);
-          myD3DImage.SetBackBuffer (D3DResourceType.IDirect3DSurface9, myColorSurf);
-        }
-        myD3DImage.Unlock ();
-      }
-    }
+            if (m_IsFailed) return;
 
-    public D3DImage Image
-    {
-      get
-      {
-        return myD3DImage;
-      }
+            if (m_D3DImage.IsFrontBufferAvailable)
+            {
+                Viewer = new OCCViewer();
+
+                if (!Viewer.InitViewer())
+                {
+                    // TODO: handle error
+                    MessageBox.Show("Failed to initialize OpenGL-Direct3D interoperability!",
+                      "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    m_IsFailed = true;
+                    return;
+                }
+
+                // Leverage the Rendering event of WPF composition
+                // target to update the our custom Direct3D scene
+                CompositionTarget.Rendering += OnRendering;
+            }
+        }
+
+        /// <summary> Releases Direct3D-OCCT rendering. </summary>
+        public void StopRenderingScene()
+        {
+            // This method is called when WPF loses its Direct3D device,
+            // so we should just release our custom Direct3D scene
+            CompositionTarget.Rendering -= OnRendering;
+            m_ColorSurf = IntPtr.Zero;
+        }
+
+        /// <summary> Performs Direct3D-OCCT rendering. </summary>
+        private void OnRendering(object? sender, EventArgs e)
+        {
+            UpdateScene();
+        }
+
+        /// <summary> Performs Direct3D-OCCT rendering. </summary>
+        private void UpdateScene()
+        {
+            if (!m_IsFailed
+              && m_D3DImage.IsFrontBufferAvailable
+              && m_ColorSurf != IntPtr.Zero
+              && (m_D3DImage.PixelWidth != 0 && m_D3DImage.PixelHeight != 0)
+              && Viewer != null)
+            {
+                m_D3DImage.Lock();
+                {
+                    // Update the scene (via a call into our custom library)
+                    Viewer.View.RedrawView();
+
+                    // Invalidate the updated region of the D3DImage
+                    m_D3DImage.AddDirtyRect(new Int32Rect(0, 0, m_D3DImage.PixelWidth, m_D3DImage.PixelHeight));
+                }
+                m_D3DImage.Unlock();
+            }
+        }
+
+        /// <summary> Resizes Direct3D surfaces and OpenGL FBO. </summary>
+        public void Resize(int theSizeX, int theSizeY)
+        {
+            if (!m_IsFailed && m_D3DImage.IsFrontBufferAvailable && Viewer != null)
+            {
+                // Set the back buffer for Direct3D WPF image
+                m_D3DImage.Lock();
+                {
+                    m_D3DImage.SetBackBuffer(D3DResourceType.IDirect3DSurface9, IntPtr.Zero);
+                    m_ColorSurf = Viewer.View.ResizeBridgeFBO(theSizeX, theSizeY);
+                    m_D3DImage.SetBackBuffer(D3DResourceType.IDirect3DSurface9, m_ColorSurf);
+                }
+                m_D3DImage.Unlock();
+            }
+        }
     }
-  }
 }
--- OccViewer.Viewer/MainWindow.xaml.cs
+++ OccViewer.Viewer/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using IE_WPF_D3D;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -28,8 +27,6 @@ namespace OccViewer.Viewer
 
         private D3DViewer? D3DViewer { get; set; }
 
-        private Grid? ViewerGrid { get; set; }
-
         protected void RaisePropertyChanged(string thePropertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(thePropertyName));
@@ -45,53 +42,117 @@ namespace OccViewer.Viewer
         private void InitViewer()
         {
             D3DViewer = new D3DViewer();
-            Grid g = new Grid();
-            g.Background = new SolidColorBrush(Colors.Black);
-            ImageBrush anImage = new ImageBrush(D3DViewer.Image);
-            g.Background = anImage;
-            g.MouseMove += new MouseEventHandler(g_MouseMove);
-            g.MouseDown += new MouseButtonEventHandler(g_MouseDown);
-            g.MouseUp += new MouseButtonEventHandler(g_MouseUp);
-            g.SizeChanged += new SizeChangedEventHandler(g_SizeChanged);
-            g.HorizontalAlignment = HorizontalAlignment.Stretch;
-            ViewerRoot.Children.Add(g);
-            ViewerGrid = g;
+            ImageBrush anImage = new(D3DViewer.Image);
+            ViewerGrid.Background = anImage;
             ActiveViewer = D3DViewer.Viewer;
         }
 
-        private void g_SizeChanged(object sender, SizeChangedEventArgs e)
+        private void ViewerGrid_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             D3DViewer?.Resize(Convert.ToInt32(e.NewSize.Width),
                               Convert.ToInt32(e.NewSize.Height));
         }
 
-        void g_MouseUp(object sender, MouseButtonEventArgs e)
+        void ViewerGrid_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (ViewerGrid != null)
-            {
-                ActiveViewer?.OnMouseUp(ViewerGrid, e);
-            }
+            ActiveViewer?.OnMouseUp(ViewerGrid, e);
         }
 
-        void g_MouseDown(object sender, MouseButtonEventArgs e)
+        void ViewerGrid_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (ViewerGrid != null)
-            {
-                ActiveViewer?.OnMouseDown(ViewerGrid, e);
-            }
+            ActiveViewer?.OnMouseDown(ViewerGrid, e);
         }
 
-        void g_MouseMove(object sender, MouseEventArgs e)
+        void ViewerGrid_MouseMove(object sender, MouseEventArgs e)
         {
-            if (ViewerGrid != null)
-            {
-                ActiveViewer?.OnMouseMove(ViewerGrid, e);
-            }
+
+            ActiveViewer?.OnMouseMove(ViewerGrid, e);
+
         }
 
         private void Import_Click(object sender, RoutedEventArgs e)
         {
             ActiveViewer?.ImportModel(ModelFormat.IGES);
+        }
+
+        private void BtnFitAll_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.FitAll();
+        }
+
+        private void BtnZoomWindow_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.ZoomWindow();
+        }
+
+        private void BtnDynamicZoom_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.DynamicZooming();
+        }
+
+        private void BtnPan_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.DynamicPanning();
+        }
+
+        private void BtnRotate_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.DynamicRotation();
+        }
+
+        private void BtnFront_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.FrontView();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.BackView();
+        }
+
+        private void BtnTop_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.TopView();
+        }
+
+        private void BtnBottom_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.BottomView();
+        }
+
+        private void BtnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.LeftView();
+        }
+
+        private void BtnRight_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.RightView();
+        }
+
+        private void BtnWireframe_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.Wireframe();
+        }
+
+        private void BtnShade_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveViewer?.Shading();
+        }
+
+        private void BtnSetColor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSetMaterial_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSetTransparency_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
